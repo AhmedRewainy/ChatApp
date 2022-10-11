3bd0603ac074e2312f2abac9ddab62457cd74570
Ahmed Rewainy <arewainy2@gmail.com>
2022-10-11 16:54:19 +0200

    First commit

---
+++ php/config.php
@@ -0,0 +1,10 @@
+<?php
+  $hostname = "localhost";
+  $username = "root";
+  $password = "";
+  $dbname = "chatapp";
+
+  $conn = mysqli_connect("localhost", "root", "", "bot") or die("Database Error");
+
+?>
+
